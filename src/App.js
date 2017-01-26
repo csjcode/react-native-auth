43a44c1d605eddfe9fb13e61d9b67c2a232ff0b1
csjcode <christophersstjohn@gmail.com>
2017-01-26 21:54:41 +0700

    Edit Text out

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import firebase from 'firebase';
 import { Header } from './components/common';
 import { LoginForm } from './components/LoginForm';
